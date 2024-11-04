9de81f910f5eaa6208022414b6b73fbb15355a77
wanghairui <wanghairui@xcastle.cn>
2024-11-04 18:07:45 +0800

    数字人项目第一个版本，初次建立

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, 
+    static_folder='public',  # 设置静态文件夹
+    static_url_path=''       # 设置URL路径前缀为空
+)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True) 
